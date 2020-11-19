a8491720c7fdf98fda43c35bfb545527ba2a4f84
devkeita <keita.mywork@gmail.com>
2020-11-19 12:24:37 +0900

    make SignOut component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Auth from "../components/Auth";
+import SignOut from "../components/SignOut";
 
 const Index: React.FC = () => {
     return (
         <Auth>
             <h1>施工管理アプリ</h1>
+            <SignOut />
         </Auth>
     );
 };
