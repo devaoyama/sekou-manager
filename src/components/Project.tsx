731c385ec08c45b081c7a68f9248b44daeaef155
devkeita <keita.mywork@gmail.com>
2020-11-19 17:57:14 +0900

    make Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { Card, CardMedia, CardContent, CardActionArea, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -12,24 +13,31 @@ const useStyles = makeStyles({
     },
 });
 
-const Project = () => {
+interface IProps {
+    id: string
+    name: string
+}
+
+const Project: React.FC<IProps> = ({ id, name }) => {
     const classes = useStyles();
 
     return (
         <React.Fragment>
             <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.media}
-                        image="http://lorempixel.com/400/200"
-                        title="プロジェクト画像"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            プロジェクト名
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
+                <Link href="/projects/[id]" as={`/projects/${id}`}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media}
+                            image="http://lorempixel.com/400/200"
+                            title="プロジェクト画像"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {name}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Link>
             </Card>
         </React.Fragment>
     );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { Grid } from "@material-ui/core";
 import Project from "./Project";
+import { db } from "../utils/Firebase";
+import { AuthContext } from "../context/Auth";
 
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
+
+    const { currentUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        db.collection('projects').where('user', '==', currentUser.uid).get()
+            .then(snapshot => {
+                let data = [];
+                snapshot.forEach(doc => {
+                    data.push({id: doc.id, ...doc.data()});
+                });
+                setProjects(data);
+            })
+        ;
+    }, []);
+
     return (
         <React.Fragment>
             <Grid container spacing={3}>
-                <Grid item md={4} sm={6} xs={12}>
-                    <Project />
-                </Grid>
-                <Grid item md={4} sm={6} xs={12}>
-                    <Project />
-                </Grid>
-                <Grid item md={4} sm={6} xs={12}>
-                    <Project />
-                </Grid>
+                {projects.map(project => {
+                    return (
+                        <Grid item md={4} sm={6} xs={12} key={project.id}>
+                            <Project name={project.name} id={project.id} />
+                        </Grid>
+                    );
+                })}
             </Grid>
         </React.Fragment>
     );
