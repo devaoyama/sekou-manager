002ff9b462167385d1bd048ac6b6498ab5dd0b5a
devkeita <keita.mywork@gmail.com>
2020-12-09 22:47:28 +0900

    remove blueimp-load-image library

--- src/utils/resizeImage.ts
+++ src/utils/resizeImage.ts
@@ -1,9 +1,2 @@
-import loadImage from 'blueimp-load-image';
-
 export const resizeImage = (file) => {
-    return new Promise((resolve) => {
-        loadImage(file, (canvas: HTMLCanvasElement) => {
-            resolve(canvas.toDataURL('image/jpg'));
-        }, { maxWidth: 1920, canvas: true });
-    });
 };
